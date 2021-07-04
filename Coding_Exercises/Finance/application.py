7d51c16b445413aa01dca82b47afc590ff499ea2
Luis <ramirez.luisraf@gmail.com>
2021-07-03 22:39:20 -0400

    2nd change

--- Coding_Exercises/Finance/application.py
+++ Coding_Exercises/Finance/application.py
@@ -119,20 +119,32 @@ def quote():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    # User reached route via POST (as by submitting a form via POST)
+
     if request.method == "POST":
 
-        # if not request.form.get("username"):
-        #     return apology("must provide username", 403)
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        elif request.form['password'] != request.form['confirmation']:
+            return apology("password and confirmation password do not match", 403)
 
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        if len(rows) != 1:
-            return apology("username already exists",409)
+        if len(rows) != 0:
+            return apology("username already exists", 403)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",username=request.form.get("username"), hash=generate_password_hash(request.form.get("password")))
+
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
-    """Register user"""
-    return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
